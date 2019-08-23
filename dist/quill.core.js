3f40aed18b6b9a8bfdb07e2301723b845a5f098c
Jason Yin <lyin512@aucklanduni.ac.nz>
2019-08-23 13:51:26 +1200

    Build

--- dist/quill.core.js
+++ dist/quill.core.js
@@ -4910,16 +4910,14 @@ function handleDelete(range, context) {
 }
 
 function handleDeleteRange(range) {
-  var lines = this.quill.getLines(range);
-  var formats = {};
-  if (lines.length > 1) {
-    var firstFormats = lines[0].formats();
-    var lastFormats = lines[lines.length - 1].formats();
-    formats = _op2.default.attributes.diff(lastFormats, firstFormats) || {};
-  }
+  var formats = this.quill.getFormat(range.index);
   this.quill.deleteText(range, _quill2.default.sources.USER);
   if (Object.keys(formats).length > 0) {
-    this.quill.formatLine(range.index, 1, formats, _quill2.default.sources.USER);
+    for (var key in formats) {
+      if (Object.prototype.hasOwnProperty.call(formats, key)) {
+        this.quill.format(key, formats[key]);
+      }
+    }
   }
   this.quill.setSelection(range.index, _quill2.default.sources.SILENT);
   this.quill.focus();
